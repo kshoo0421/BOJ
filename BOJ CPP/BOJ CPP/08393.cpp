46aa1a88911475c828614f99aa8f42ee0be17ba5
kshoo0421 <shmd02@google.com>
2022-11-06 00:00:45 +0900

    22.11.05 BOJ
    .

---
+++ BOJ CPP/BOJ CPP/08393.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, sum = 0;
+	cin >> n;
+	while (n != 0)
+	{
+		sum += n--;
+	}
+	cout << sum;
+	return 0;
+}
---
+++ BOJ CPP/BOJ CPP/11021.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, a, b, num = 1;
+	cin >> t;
+	while (num <= t)
+	{
+		cin >> a >> b;
+		cout << "Case #" << num << ": " << a + b << endl;
+		num++;
+	}
+	return 0;
+}
