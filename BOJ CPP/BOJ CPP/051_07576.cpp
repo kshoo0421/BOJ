1928c512fc6c265b85bb9a54f649c775ff860517
kshoo0421 <shmd02@google.com>
2024-06-16 02:51:12 +0900

    24.06.16
    .

--- BOJ CPP/BOJ CPP/051_07576.cpp
+++ BOJ CPP/BOJ CPP/051_07576.cpp
@@ -1,75 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int dx[] = { 1, 0, -1, 0 };
+int dy[] = { 0, 1, 0, -1 };
+
 int main()
 {
-	ios::sync_with_stdio(0), cin.tie(0);
-	int m, n, tmp_x, tmp_y, cur_x, cur_y, max = 0;
-	int x_idx[4] = { 1, 0, -1, 0 }, y_idx[4] = { 0, 1, 0, -1 };
-	vector<int> tmp_vc;
-	queue<pair<int, int>> q, tomato;
+    ios::sync_with_stdio(0), cin.tie(0);
+    int m, n, total = 0, done = 0, days = -1, size, ny, nx;
+    cin >> m >> n;
 
-	cin >> n >> m;
-	vector<vector<int>> input(m), memo(m), visit(m);
-	tmp_vc.assign(n, -1);
-	for (auto& i : input) i = tmp_vc;
-	for (auto& i : memo) i = tmp_vc;
-	for (auto& i : visit) i = tmp_vc;
-	for (auto& i : input) for (int& j : i) cin >> j;
+    vector<vector<int>> vvi(n, vector<int>(m));
+    queue<pair<int, int>> q;
 
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (input[i][j] == -1) memo[i][j] = 0;
-			if (input[i][j] == 1)
-			{
-				memo[i][j] = 0;
-				q.push({ i, j });
-			}
-		}
-	}
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> vvi[i][j];
+            if (vvi[i][j] == 1) {
+                q.push({ i, j });
+                done++;
+            }
+            if (vvi[i][j] != -1) total++;
+        }
+    }
 
-	while (!(q.empty()))
-	{
-		tmp_x = q.front().second;
-		tmp_y = q.front().first;
-		q.pop();
-		for (int k = 0; k < 4; k++)
-		{
-			cur_x = tmp_x + x_idx[k];
-			cur_y = tmp_y + y_idx[k];
-			if (cur_x < 0 || cur_x >= n) continue;
-			if (cur_y < 0 || cur_y >= m) continue;
-			if (visit[cur_y][cur_x] == 1) continue;
+    if (done == total) {
+        cout << "0\n";
+        return 0;
+    }
 
-			if (input[cur_y][cur_x] == 0)
-			{
-				visit[cur_y][cur_x] = 1;
-				q.push({ cur_y, cur_x });
-				if (memo[cur_y][cur_x] == -1)
-					memo[cur_y][cur_x] = memo[tmp_y][tmp_x] + 1;
-				else
-					memo[cur_y][cur_x] = min(memo[cur_y][cur_x],
-						memo[tmp_y][tmp_x] + 1);
-			}
-		}
-	}
+    while (!q.empty()) {
+        size = q.size();
+        days++;
+        for (int i = 0; i < size; i++) {
+            auto cycx = q.front();
+            q.pop();
+            for (int dir = 0; dir < 4; dir++) {
+                ny = cycx.first + dy[dir];
+                nx = cycx.second + dx[dir];
+                if (ny >= 0 && ny < n && nx >= 0 && nx < m && vvi[ny][nx] == 0) {
+                    vvi[ny][nx] = 1;
+                    q.push({ ny, nx });
+                    done++;
+                }
+            }
+        }
+    }
 
-	for (const auto& i : memo)
-	{
-		for (const auto& j : i)
-		{
-			if (j == -1)
-			{
-				max = -1;
-				break;
-			}
-			if (j > max) max = j;
-		}
-		if (max == -1) break;
-	}
-	cout << max;
-
-	return 0;
+    if (done == total) cout << days << endl;
+    else cout << "-1\n";
+    return 0;
 }
--- BOJ CPP/BOJ CPP/071_10026.cpp
+++ BOJ CPP/BOJ CPP/071_10026.cpp
@@ -1,86 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
-int first[101][101], second[101][101], cost[101][101];
-int n, cx, cy, nx, ny, cc, cnt = 0;
-int dx[4] = { 1, 0, -1, 0 }, dy[4] = { 0, 1, 0, -1 };
-stack<pair<int, int>> stk;
 
-int main()
-{
-	ios::sync_with_stdio(0), cin.tie(0);
-	string input;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> input;
-		for (int j = 0; j < n; j++)
-		{
-			if (input[j] == 'R') first[i][j] = 1, second[i][j] = 1;
-			else if (input[j] == 'G') first[i][j] = 2, second[i][j] = 1;
-			else first[i][j] = 3, second[i][j] = 3;
-		}
-	}
+int dx[] = { 1, 0, -1, 0 };
+int dy[] = { 0, 1, 0, -1 };
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (cost[i][j] == 0)
-			{
-				cnt++;
-				cc = first[i][j];
-				stk.push({ i, j });
-				while (!stk.empty())
-				{
-					cx = stk.top().second;
-					cy = stk.top().first;
-					stk.pop();
-					cost[cy][cx] = 1;
-					for (int k = 0; k < 4; k++)
-					{
-						nx = cx + dx[k];
-						ny = cy + dy[k];
-						if (nx < 0 || nx >= n) continue;
-						if (ny < 0 || ny >= n) continue;
-						if (first[ny][nx] != cc) continue;
-						if (cost[ny][nx] == 1) continue;
-						stk.push({ ny, nx });
-					}
-				}
-			}
-		}
-	}
-	cout << cnt << " ";
-	cnt = 0;
-	fill(cost[0], cost[0] + (int)pow(101, 2), 0);
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (cost[i][j] == 0)
-			{
-				cnt++;
-				cc = second[i][j];
-				stk.push({ i, j });
-				while (!stk.empty())
-				{
-					cx = stk.top().second;
-					cy = stk.top().first;
-					stk.pop();
-					cost[cy][cx] = 1;
-					for (int k = 0; k < 4; k++)
-					{
-						nx = cx + dx[k];
-						ny = cy + dy[k];
-						if (nx < 0 || nx >= n) continue;
-						if (ny < 0 || ny >= n) continue;
-						if (second[ny][nx] != cc) continue;
-						if (cost[ny][nx] == 1) continue;
-						stk.push({ ny, nx });
-					}
-				}
-			}
-		}
-	}
-	cout << cnt;
+int n, cx, cy, nx, ny;
+vector<string> grid;
+vector<vector<bool>> visited;
+
+void dfs(int x, int y, char color, bool isBlind) {
+    stack<pair<int, int>> stk;
+    stk.push({ x, y });
+    visited[x][y] = true;
+
+    while (!stk.empty()) {
+        cy = stk.top().first;
+        cx = stk.top().second;
+        stk.pop();
+        for (int i = 0; i < 4; i++) {
+            nx = cx + dx[i];
+            ny = cy + dy[i];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[ny][nx]) {
+                if (isBlind) {
+                    if ((color == 'R' || color == 'G')
+                        && (grid[ny][nx] == 'R' || grid[ny][nx] == 'G')) {
+                        visited[ny][nx] = true;
+                        stk.push({ ny, nx });
+                    }
+                    else if (color == 'B' && grid[ny][nx] == 'B') {
+                        visited[ny][nx] = true;
+                        stk.push({ ny, nx });
+                    }
+                }
+                else {
+                    if (grid[ny][nx] == color) {
+                        visited[ny][nx] = true;
+                        stk.push({ ny, nx });
+                    }
+                }
+            }
+        }
+    }
+}
+
+int getCnt(bool isBlind) {
+    visited.assign(n, vector<bool>(n, false));
+    int regions = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!visited[i][j]) {
+                regions++;
+                dfs(i, j, grid[i][j], isBlind);
+            }
+        }
+    }
+    return regions;
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> n;
+    grid.resize(n);
+    for (int i = 0; i < n; i++) cin >> grid[i];
+    cout << getCnt(false) << " " << getCnt(true);
+    return 0;
 }
